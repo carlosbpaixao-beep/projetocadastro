3ec106125f618724273be35d39bfe0ade80f6a60
carlosbpaixao-beep <carlosbpaixao@gmail.com>
2025-09-01 20:49:29 -0300

    Create validators.js

---
+++ validators.js
@@ -0,0 +1,10 @@
+export const STATUS = { PENDENTE: "Pendente", APROVADO: "Aprovado", REPROVADO: "Reprovado" };
+
+// Validador CNPJ
+export function validCNPJ(raw) { /* função completa que você já tem */ }
+
+// Validador e-mail
+export function validEmail(email) { /* função completa que você já tem */ }
+
+// Formata CNPJ
+export function formatCNPJ(value) { /* função completa que você já tem */ }
